293049b3119438d0e7631e5c3d71fbcd7212f6b6
chris <chrisabelw@m3.com>
2023-01-26 14:05:33 -0500

    Added Book class

---
+++ Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Group_2_Library
+{
+	public class Book
+	{
+		//Properties
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public string Genre { get; set; }
+		public bool Available { get; set; }
+		public DateTime DueDate { get; set; }
+
+		//Constructors
+		public Book(string _title, string _author, string _genre)
+		{
+			Title = _title;
+			Author = _author;
+			Genre = _genre;
+			Available = true;
+			DueDate = DateTime.Today;
+		}
+
+		//Methods
+		public string GetDetails()
+		{
+			return $"Title: {Title}\tAuthor: {Author}\tGenre: {Genre}\tStatus: {Available}\tDueDate: {DueDate}";
+		}
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,3 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Group_2_Library;
 
