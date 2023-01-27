2299b37f6d55383f309621437a519e70ff26b9e5
john <glandonj@gmail.com>
2023-01-27 17:00:25 -0500

    updated with BookSelection methods

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ using static System.Reflection.Metadata.BlobBuilder;
 using Validation;
 using System.ComponentModel;
 
-//Validator.GetContinue();
-
 //Book list
 List<Book> ourBooks = new List<Book>()
 {
@@ -22,9 +20,6 @@ List<Book> ourBooks = new List<Book>()
     new Book("The Hobbit", "J. R. R. Tolkien", "Fantasy"),
     new Book("The Lion, the Witch and the Wardrobe", "C. S. Lewis", "Fantasy")
 };
-
-//Adding days when checking out book
-//DateTime addedDateTime = DateTime.Now.AddDays(14)
 
 List<Book> bag = new List<Book>();
 
@@ -46,13 +41,13 @@ while (continueQ)
 
         if (choice == "l")
         {
-            //list all book info (done)
+            //list all book info
             PrintHeader();
             foreach (Book b in ourBooks.OrderBy(b => b.Title))
             {
                 Console.WriteLine(b.GetDetails());
             }
-            Console.WriteLine("Please enter a title -- test method");
+
             int selectedBook = BookSelection(ourBooks);
             bag.Add(ourBooks[selectedBook]);
             ourBooks[selectedBook].UpdateDueDate();
@@ -83,41 +78,14 @@ while (continueQ)
                     }
                     Console.WriteLine();
 
-                    //targeting book choice
-                    while (true)
-                    {
-                        Console.WriteLine("Please enter the title you'd like to check out:");
-                        string bchoice = Console.ReadLine().Trim().ToLower();
-                        int indexChoice = ourBooks.FindIndex(b => b.Title.ToLower().Contains(bchoice));
+                    //targeting book choice + checkout
 
-                        if (indexChoice == -1 || indexChoice > ourBooks.Count)
-                        {
-                            Console.WriteLine("Book not found. Please re-enter title.");
-                        }
-                        else
-                        {
-                            Console.Clear();
-                            if (ourBooks[indexChoice].Available == true)
-                            {
-                                Console.WriteLine($"You're checking out: {ourBooks[indexChoice].Title}");
-                                bag.Add(ourBooks[indexChoice]);
-                                ourBooks[indexChoice].UpdateDueDate();
-                                //FUNCTIONALITY FOR CHECK-OUT
+                    int selectedBook = BookSelection(ourBooks);
+                    bag.Add(ourBooks[selectedBook]);
+                    ourBooks[selectedBook].UpdateDueDate();
 
-                                stillSearching = false;
-                                stillWorking = false;
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Sorry, {ourBooks[indexChoice].Title} is already checked out. Please select another book");
-                                stillSearching = false;
-                                stillWorking = false;
-                                break;
-                            }
-
-                        }
-                    }
+                    stillSearching = false;
+                    stillWorking = false;
                 }
                 else if (searchType == "k")
                 {
@@ -132,57 +100,29 @@ while (continueQ)
                     }
                     Console.WriteLine();
 
-                    //targeting book choice
-                    while (true)
-                    {
-                        Console.WriteLine("Please enter the title you'd like to check out:");
-                        string bchoice = Console.ReadLine().Trim().ToLower();
-                        int indexChoice = ourBooks.FindIndex(b => b.Title.ToLower().Contains(bchoice));
+                    //targeting book choice + checkout
 
-                        if (indexChoice == -1 || indexChoice > ourBooks.Count)
-                        {
-                            Console.WriteLine("Book not found. Please re-enter title.");
-                        }
-                        else
-                        {
-                            Console.Clear();
-                            if (ourBooks[indexChoice].Available == true)
-                            {
-                                //checkout
-                                Console.WriteLine($"You're checking out: {ourBooks[indexChoice].Title}");
-                                bag.Add(ourBooks[indexChoice]);
-                                ourBooks[indexChoice].UpdateDueDate();
-                                stillSearching = false;
-                                stillWorking = false;
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Sorry, {ourBooks[indexChoice].Title} is already checked out. Please select another book");
-                                stillSearching = false;
-                                stillWorking = false;
-                                break;
-                            }
-                        }
-                        stillWorking = false;
-                        break;
-                    }
+                    int selectedBook = BookSelection(ourBooks);
+                    bag.Add(ourBooks[selectedBook]);
+                    ourBooks[selectedBook].UpdateDueDate();
+
+                    stillSearching = false;
+                    stillWorking = false;
                 }
 
                 else
                 {
-                    //validate choice - search by author or keyword (done)
+                    //validate choice - search by author or keyword
                     Console.WriteLine("Invalid input, please try again.");
                 }
             }
         }
-        else //validate choice - list or search (done)
+        else //validate choice - list or search
         {
             Console.WriteLine("Invalid input, please try again.");
         }
     }
-
-    //Validator.GetContinueCustom();
+// Asking to continue
     while (continueQ)
     {
         Console.WriteLine();
@@ -221,7 +161,7 @@ while (true)
         Console.WriteLine("Which book would you like to return?");
         string bookToReturn = Console.ReadLine().Trim();
         int returnIndex = bag.FindIndex(b => b.Title.ToLower().Contains(bookToReturn));
-        try// When the user types in a book that doesn't exist or is not in their bag, it will return an exception 
+        try// When the user types in a book that doesn't exist or is not in their bag, it will return an exception
         {
             if (bag[returnIndex].Available == false)
             {
@@ -238,9 +178,6 @@ while (true)
         {
             Console.WriteLine("Book was not found checked out.");
         }
-        
-        //identify index # of book to return
-        //flip available bool
         
         break;
     }
@@ -266,6 +203,8 @@ foreach (Book b in bag)
 Console.WriteLine("\nGoodbye.");
 Console.ReadLine();
 
+
+//Methods
 static int BookSelection(List<Book>books)
 {
     
@@ -281,7 +220,7 @@ static int BookSelection(List<Book>books)
         }
         else
         {
-            
+            Console.WriteLine($"You're checking out: {books[indexChoice].Title}");
             return indexChoice;
             
         }
@@ -290,12 +229,10 @@ static int BookSelection(List<Book>books)
     return -1;
     
 }
- 
 
 
 
-
-            static void PrintHeader()
+static void PrintHeader()
 {
     Console.WriteLine(String.Format("{0,-40} {1,-25} {2,-20} {3, -15} {4, -15}", $"Title", $"Author", $"Genre", $"Available", $"DueDate"));
     Console.WriteLine(String.Format("{0,-40} {1,-25} {2,-20} {3, -15} {4, -15}", "========", "========", "========", "========", "========"));
